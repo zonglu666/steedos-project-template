7837b46a0e923ab4bb1002325616e31a584e1e3f
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2021-02-16 23:57:41 +0800

    node red app

--- services/steedos.service.js
+++ services/steedos.service.js
@@ -1,12 +1,37 @@
-// api.service.js
-const SteedosService = require("@steedos/service-steedos");
+const SteedosService = require("@steedos/service-steedos-server");
+const path = require('path');
 
 module.exports = {
-    name: "steedos",
+    name: "steedos-server",
     mixins: [SteedosService],
     settings: {
         // Change port setting
 		port: process.env.PORT,
-		rootUrl: process.env.ROOT_URL 
+        rootUrl: process.env.ROOT_URL,
+		mongoUrl: process.env.MONGO_URL,
+        mongoServer: {
+			enabled: !process.env.MONGO_URL,
+            port: process.env.MONGO_PORT || 27018,
+            dbPath: process.env.MONGO_DBPATH || path.join(process.cwd(), 'db'),
+        },
+        nodeRedServer: {
+			enabled: true,
+            userDir: path.join(process.cwd(), "node-red-app"),
+        },
+        apiServer: {
+			enabled: true,
+            routes: [
+                {
+                    path: "/services/",
+                    whitelist: [
+                        "greeter.*",
+                    ],
+                    aliases: {
+                        "GET /hi": "greeter.hello"
+                    },
+                    autoAliases: true
+                }
+            ]
+        }
     }
 }
